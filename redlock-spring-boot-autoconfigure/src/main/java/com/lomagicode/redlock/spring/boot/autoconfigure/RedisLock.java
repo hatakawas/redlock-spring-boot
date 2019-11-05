2d1fef938b44a3518b6f9aa0f6d7a8630bf849f9
qinchuan13 <qinchuan@jd.com>
2019-11-05 21:53:36 +0800

    license

--- redlock-spring-boot-autoconfigure/src/main/java/com/lomagicode/redlock/spring/boot/autoconfigure/RedisLock.java
+++ redlock-spring-boot-autoconfigure/src/main/java/com/lomagicode/redlock/spring/boot/autoconfigure/RedisLock.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.lomagicode.redlock.spring.boot.autoconfigure;
 
 import java.lang.annotation.ElementType;
